4a022bfaa18954bfe63848c9315d63aa0a7bb9a3
Amit Kumar Jaiswal <amitkumarj441@gmail.com>
2017-08-19 20:15:27 +0530

    bitcoin methods

---
+++ domain/src/main/scala/BitcoinCryptography.scala
@@ -0,0 +1,13 @@
+package org.parsec.protocol
+
+import org.apache.commons.codec.binary.Hex
+import org.bitcoinj.core.{ECKey, NetworkParameters}
+
+trait BitcoinCryptography {
+  def bitcoinAddressFromPublicKey(key: String): String = {
+    val ECkey = ECKey.fromPublicOnly(Hex.decodeHex(key.toCharArray))
+    val netParams = NetworkParameters.prodNet() // Generate only production addresses
+    val addressFromKey = ECkey.toAddress(netParams)
+    addressFromKey.toString
+  }
+}
